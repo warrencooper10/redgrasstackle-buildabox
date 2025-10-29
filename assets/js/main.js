a906d32ac8332c02107363f1dc58126a6bf66404
warrencooper10 <warrencooper10@gmail.com>
2025-10-29 02:17:35 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@
   const DATA_URL = BASE + 'data/baits.json';
 
   // Big Cartel bridge page
-  const CART_BRIDGE_URL = 'https://www.redgrasstackle.com/page/box-add';
+  const CART_BRIDGE_URL = 'https://www.redgrasstackle.com/pages/box-add';
 
   const GALLERY = document.getElementById('gallery');
   const SLOTS = document.getElementById('slots');
